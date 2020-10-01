704e907d85f35f4e29b11943e392bbc5f8123985
MarcoRivera503 <marco.umana@catolica.edu.sv>
2020-10-01 16:08:44 -0600

    Actividad de splash y princpal en blanco

---
+++ app/src/main/java/sv/edu/catolica/medicationreminder/Splash_inicio.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.medicationreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_inicio);
+    }
+}
