c8ba9cf47c47be4fcedb407c42c46c8a66d08842
Marco Rivera <marco.umanaatolica.edu.svgit config --global user.email marco.umanaatolica.edu.sv>
2020-10-06 14:44:42 -0600

    Agregada la pantalla de seleccionar medicamento, medicina por recordatorio y tomar medicamentos

---
+++ app/src/main/java/sv/edu/catolica/medicationreminder/Elegir_medicamento.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.medicationreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Elegir_medicamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elegir_medicamento);
+    }
+}
