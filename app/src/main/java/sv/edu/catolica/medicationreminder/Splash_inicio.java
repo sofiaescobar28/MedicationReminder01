f401d0d13cd750297a5bbcca6126b989265fda17
sofiaescobar28 <josselyn.escobar@catolica.edu.sv>
2020-10-19 18:02:55 -0600

    splash listo

--- app/src/main/java/sv/edu/catolica/medicationreminder/Splash_inicio.java
+++ app/src/main/java/sv/edu/catolica/medicationreminder/Splash_inicio.java
@@ -1,14 +1,38 @@
 package sv.edu.catolica.medicationreminder;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_inicio extends AppCompatActivity {
+    private boolean botonBackPresionado = false;
+    private static final int DURACION_SPLASH = 3000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_inicio);
+
+        Handler controlador = new Handler();
+        controlador.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                if (!botonBackPresionado){
+                    Intent principal = new Intent(Splash_inicio.this, Principal.class);
+                    startActivity(principal);
+                }
+            }
+        }, DURACION_SPLASH);
+    }
+
+    @Override
+    public void onBackPressed() {
+        botonBackPresionado=true;
+        super.onBackPressed();
     }
 }
